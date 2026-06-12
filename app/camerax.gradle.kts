ebb1583df2fb665a87df83520a9a53cd01cfa535
DeCode39 <89783656+DeCode39@users.noreply.github.com>
2026-06-12 19:41:20 +0800

    Add CameraX dependency bundle

---
+++ app/camerax.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    val cameraXVersion = "1.4.2"
+    add("implementation", "androidx.camera:camera-core:$cameraXVersion")
+    add("implementation", "androidx.camera:camera-camera2:$cameraXVersion")
+    add("implementation", "androidx.camera:camera-lifecycle:$cameraXVersion")
+    add("implementation", "androidx.camera:camera-view:$cameraXVersion")
+}
